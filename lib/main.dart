ef4c4f40975803b60a19d5ffc570c619bc4ffcb7
sajumaria <mariasajutherattil@gmail.com>
2023-04-16 21:33:19 +0530

    added homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:farespy/HomePage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold()
+      debugShowCheckedModeBanner: false,
+      home: HomePage()
     );
   }
 }
