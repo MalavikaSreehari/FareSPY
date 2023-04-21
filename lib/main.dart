e43b43bbd549b43a01c7395f03c8077a28ffb4d8
jacinjoy <jacinjoy1@gmail.com>
2023-04-21 12:30:44 +0530

    payment page one

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:farespy/HomePage.dart';
 import 'package:flutter/material.dart';
+import 'package:farespy/payment1.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage()
+      home: PaymentOne()
     );
   }
 }
