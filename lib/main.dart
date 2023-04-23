783301d9e26276fc616aface5b665b8fccd8125a
jacinjoy <jacinjoy1@gmail.com>
2023-04-23 20:23:21 +0530

    Local Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:farespy/HomePage.dart';
-import 'package:farespy/paymenttwo.dart';
 import 'package:flutter/material.dart';
-import 'package:farespy/paymentone.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PaymentTwo()
+      home: HomePage()
     );
   }
 }
