910f1df6e151204ea706654a289098736a1cfc3b
MalavikaSreehari <malavikasreehari4@gmail.com>
2023-05-03 10:17:22 +0530

    email authentication successful

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,18 @@ import 'package:farespy/initial_page.dart';
 import 'package:farespy/auto.dart';
 import 'package:farespy/login.dart';
 import 'package:farespy/signup.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:farespy/paymentone.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();  
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
+
+DatabaseReference usersRef = FirebaseDatabase.instance.ref().child("users");
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -23,6 +29,7 @@ class MyApp extends StatelessWidget {
         SignUp.idScreen:(context) => SignUp(),
         Login.idScreen:(context) => Login(),
         InitialPage.idScreen:(context) => InitialPage(),
+        HomePage.idScreen:(context) => HomePage(),
 
       },
       home: HomePage()
